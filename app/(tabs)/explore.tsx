967cf96cf32bf2d7d20d060778ced56f558a714e
FadhlanPutra <fadhlan.putra161@smk.belajar.id>
2025-01-12 12:46:04 +0700

    chore: initial commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, SafeAreaView, View, Text, Button } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -6,92 +6,188 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import React from 'react';
+import { Entypo, Foundation } from '@expo/vector-icons';
+// import { createIconSet } from 'react-native-vector-icons';
+
+const packageJson = require('../../package.json');
+// const version = packageJson.version || 'Versi tidak ditemukan';
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+
+    <SafeAreaView>
+      <View style={styles.view}>
+        <Image source={require('../../assets/images/qq.jpg')} style={styles.reactLogo}/>
+        <View style={{marginLeft: 10, flex: 1}}>
+          <Text style={styles.text}>Rasi Bintang </Text>
+          <Text style={styles.text}>Kopi</Text>
+        </View>
+        <Text style={styles.ubah}>Ubah</Text>
+      </View>
+
+      <View style={{marginHorizontal:35}}>
+        <Text style={styles.judulDetail}>Statistik</Text>
+        <View>
+          <View style={{flexDirection:'row', justifyContent:'space-between',  marginTop:10}}>
+            <View style={{flexDirection:'row', alignItems:'center'}}>
+            <View style={{padding: 20, backgroundColor: 'red', borderRadius: 100, width: 70, height: 70}}>
+              <Entypo name='open-book' size={30} color={'white'} style={{alignItems:'center'}}/>
+            </View>
+            <View style={{marginLeft:10}}>
+              <Text>10</Text>
+              <Text>Lesson</Text>
+            </View>
+            </View>
+
+            <View style={{flexDirection:'row', alignItems:'center'}}>
+            <View style={{padding: 20, backgroundColor: 'red', borderRadius: 100, width: 70, height: 70}}>
+              <Entypo name='flash' size={30} color={'white'} style={{alignItems:'center'}}/>
+            </View>
+            <View style={{marginLeft:10}}>
+              <Text>20</Text>
+              <Text>Streak</Text>
+            </View>
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <View style={{flexDirection:'row', justifyContent:'space-between', marginTop:10}}>
+          <View style={{flexDirection:'row', alignItems:'center'}}>
+            <View style={{padding: 20, backgroundColor: 'red', borderRadius: 100, width: 70, height: 70}}>
+              <Entypo name='star-outlined' size={30} color={'white'} style={{alignItems:'center'}}/>
+            </View>
+            <View style={{marginLeft:10}}>
+              <Text>120</Text>
+              <Text>XP</Text>
+            </View>
+            </View>
+
+            <View style={{flexDirection:'row', alignItems:'center'}}>
+            <View style={{padding: 20, backgroundColor: 'red', borderRadius: 100, width: 70, height: 70}}>
+              <Foundation name='target' size={30} color={'white'} style={{alignItems:'center'}}/>
+            </View>
+            <View style={{marginLeft:10}}>
+              <Text>70%</Text>
+              <Text>Akurasi</Text>
+            </View>
+            </View>
+          </View>
+        </View>
+
+        <Text style={styles.judulDetail}>Akun</Text>
+        <View style={styles.detailView}>
+          <Text style={styles.detail}>Detail Bisnis</Text>
+          <Text>&gt;</Text>
+        </View>
+        <View style={styles.detailView}>
+          <Text style={styles.detail}>Ubah Kata Sandi</Text>
+          <Text>&gt;</Text>
+        </View>
+
+        <Text style={styles.judulDetail}>Tentang</Text>
+        <View style={styles.detailView}>
+          <Text style={styles.detail}>Keuntungan Belanja di Saraya</Text>
+          <Text>&gt;</Text>
+        </View>
+        <View style={styles.detailView}>
+          <Text style={styles.detail}>Syarat dan Ketentuan</Text>
+          <Text>&gt;</Text>
+        </View>
+        <View style={styles.detailView}>
+          <Text style={styles.detail}>Kebijakan Privasi</Text>
+          <Text>&gt;</Text>
+        </View>
+    </View>
+
+    <Text style={{fontSize:10, fontWeight:'light', color:'grey', textAlign:'center'}}>Versi APP: {packageJson.version}</Text>
+
+    </SafeAreaView>
+
+    // <ParallaxScrollView
+    //   headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+    //   headerImage={
+    //     <IconSymbol
+    //       size={310}
+    //       color="#808080"
+    //       name="chevron.left.forwardslash.chevron.right"
+    //       style={styles.headerImage}
+    //     />
+    //   }>
+    //   <ThemedView style={styles.titleContainer}>
+    //     <ThemedText type="title">Explore</ThemedText>
+    //   </ThemedView>
+    //   <ThemedText>This app includes example code to help you get started.</ThemedText>
+    //   <Collapsible title="File-based routing">
+    //     <ThemedText>
+    //       This app has two screens:{' '}
+    //       <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
+    //       <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
+    //     </ThemedText>
+    //     <ThemedText>
+    //       The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
+    //       sets up the tab navigator.
+    //     </ThemedText>
+    //     <ExternalLink href="https://docs.expo.dev/router/introduction">
+    //       <ThemedText type="link">Learn more</ThemedText>
+    //     </ExternalLink>
+    //   </Collapsible>
+    //   <Collapsible title="Android, iOS, and web support">
+    //     <ThemedText>
+    //       You can open this project on Android, iOS, and the web. To open the web version, press{' '}
+    //       <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
+    //     </ThemedText>
+    //   </Collapsible>
+    //   <Collapsible title="Images">
+    //     <ThemedText>
+    //       For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
+    //       <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
+    //       different screen densities
+    //     </ThemedText>
+    //     <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
+    //     <ExternalLink href="https://reactnative.dev/docs/images">
+    //       <ThemedText type="link">Learn more</ThemedText>
+    //     </ExternalLink>
+    //   </Collapsible>
+    //   <Collapsible title="Custom fonts">
+    //     <ThemedText>
+    //       Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
+    //       <ThemedText style={{ fontFamily: 'SpaceMono' }}>
+    //         custom fonts such as this one.
+    //       </ThemedText>
+    //     </ThemedText>
+    //     <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
+    //       <ThemedText type="link">Learn more</ThemedText>
+    //     </ExternalLink>
+    //   </Collapsible>
+    //   <Collapsible title="Light and dark mode components">
+    //     <ThemedText>
+    //       This template has light and dark mode support. The{' '}
+    //       <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
+    //       what the user's current color scheme is, and so you can adjust UI colors accordingly.
+    //     </ThemedText>
+    //     <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
+    //       <ThemedText type="link">Learn more</ThemedText>
+    //     </ExternalLink>
+    //   </Collapsible>
+    //   <Collapsible title="Animations">
+    //     <ThemedText>
+    //       This template includes an example of an animated component. The{' '}
+    //       <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
+    //       the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
+    //       library to create a waving hand animation.
+    //     </ThemedText>
+    //     {Platform.select({
+    //       ios: (
+    //         <ThemedText>
+    //           The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
+    //           component provides a parallax effect for the header image.
+    //         </ThemedText>
+    //       ),
+    //     })}
+    //   </Collapsible>
+    // </ParallaxScrollView>
   );
 }
 
@@ -105,5 +201,54 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
+  },
+  view: {
+    marginTop: 30,
+    padding: 50,
+    // display: 'flex',
+    // flex: 3,
+    flexDirection: 'row',
+    // justifyContent: 'space-evenly',
+    justifyContent: 'center',
+    height: 150,
+    alignItems: 'center',
+    backgroundColor: 'red', 
+    borderBottomLeftRadius: 30, 
+    borderBottomRightRadius: 30
+  },
+  reactLogo: {
+    // marginRight: 10,
+    borderRadius: 100,
+    height: 50,
+    width: 50,
+  },
+  text: {
+    color: 'white',
+    fontSize: 17,
+  },
+  ubah: {
+    textAlign: 'right',
+    color: 'white',
+    fontSize: 17,
+  },
+  judulDetail: {
+    fontSize:24,
+    fontWeight: 'bold',
+    paddingHorizontal: 5,
+    marginTop: 20,
+    // color:'red', 
+    // marginBottom:10
+  },
+  detailView: {
+    justifyContent:'space-between', 
+    flexDirection: 'row',
+    marginRight: 5,
+  },
+  detail: {
+    marginHorizontal: 10,
+    fontSize: 17,
+    // fontWeight: 'bold',
+    // color: 'white',
+    marginBottom: 10
   },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,56 +1,72 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, TextInput, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+
+    <SafeAreaView>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Image source={require('../../assets/images/qq.jpg')} style={styles.reactLogo}/>
+        <Text style={{textAlign: 'center', fontSize: 24, fontWeight: 'bold',}}>hehe</Text>
+        <Text style={{textAlign: 'center', fontSize: 19, marginTop: 10, color: 'grey'}}> "Hi mi nem is Fadhlan Putra, ai em manusia, ai em a perogramer, ai em pelajar, ai em pelaying gem, ai em kurang ingpo aku kang 😂😂"</Text>
+        <TextInput placeholder='isi pin atm' style={{borderWidth: 1, marginBottom: 20, marginTop: 20,}}></TextInput>
+        <Button
+          title="Submit"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+    </SafeAreaView>
+
+    // <ParallaxScrollView
+    //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+    //   headerImage={
+    //     <Image
+    //       source={require('@/assets/images/partial-react-logo.png')}
+    //       style={styles.reactLogo}
+    //     />
+    //   }>
+    //   <ThemedView style={styles.titleContainer}>
+    //     <ThemedText type="title">Welcome!</ThemedText>
+    //     <HelloWave />
+    //   </ThemedView>
+    //   <ThemedView style={styles.stepContainer}>
+    //     <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+    //     <ThemedText>
+    //       Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+    //       Press{' '}
+    //       <ThemedText type="defaultSemiBold">
+    //         {Platform.select({
+    //           ios: 'cmd + d',
+    //           android: 'cmd + m',
+    //           web: 'F12'
+    //         })}
+    //       </ThemedText>{' '}
+    //       to open developer tools.
+    //     </ThemedText>
+    //   </ThemedView>
+    //   <ThemedView style={styles.stepContainer}>
+    //     <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+    //     <ThemedText>
+    //       Tap the Explore tab to learn more about what's included in this starter app.
+    //     </ThemedText>
+    //   </ThemedView>
+    //   <ThemedView style={styles.stepContainer}>
+    //     <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+    //     <ThemedText>
+    //       When you're ready, run{' '}
+    //       <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+    //       <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+    //       <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+    //       <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+    //     </ThemedText>
+    //   </ThemedView>
+    // </ParallaxScrollView>
   );
 }
 
@@ -65,10 +81,15 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginBottom: 12,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // alignSelf: 'center',
+    // bottom: 0,
+    // left: 0,
+    // position: 'absolute',
   },
 });
